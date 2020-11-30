e7d954af35d644f4329a2cb25e363152cc85e6b2
Abdul Khan <akhan@lcs.on.ca>
2020-11-30 11:30:59 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Abdul Ahad Khan on 2020-11-30.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
